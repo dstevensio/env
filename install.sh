be2d3b52a515d4f79bcf738509c92a357689bf5f
Geoff Berger <gberger@zappos.com>
2010-11-06 09:29:43 -0700

    Added install script.

--- install.sh
+++ install.sh
@@ -1,1 +1,41 @@
-ln -s ~/env/
+#!/bin/bash
+
+ENV_DIR=/Users/gberger/env
+ENV_FILES=`ls -a $ENV_DIR`
+EXCLUDED_FILES=( . .. install.sh .bashrc .git )
+ENDS_WITH=( swp )
+TARGET_DIR=~/
+
+for f in $ENV_FILES; do
+  filename=$(basename $f)
+
+  if [ -f $filename -o -d $filename ]; then
+    create_symlink=true
+
+    for ef in "${EXCLUDED_FILES[@]}"; do
+      if [ $filename == $ef ]; then
+        create_symlink=false
+        break
+      fi
+    done
+
+    if $create_symlink; then
+      for ew in "${ENDS_WITH[@]}"; do
+        if [ ${filename##*.} == $ew ]; then
+          create_symlink=false
+          break
+        fi
+      done
+    fi
+
+    if $create_symlink; then
+      symlink=$TARGET_DIR$filename
+
+      if [ ! -h $symlink ]; then
+        ln -s $ENV_DIR/$f $symlink
+        echo "Symlink created for $f"
+      fi
+    fi
+  fi
+done
+
